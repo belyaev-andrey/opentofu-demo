5cee7a19aa5c93d378e9917af40610e8c91c54ef
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2024-11-06 16:40:42 +0100

    Added recursion check

--- tf_only/elem_test.tf
+++ tf_only/elem_test.tf
@@ -13,22 +13,10 @@ terraform {
     aws = {
       source = "registry.opentofu.org/hashicorp/aws"
     }
-    hashicups = {
-      source  = "app.terraform.io/custom-org/hashicups"
-      version = "0.2.3"
-    }
   }
 
 }
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "bucket"
-}
-
-provider "hashicups" {}
-
-data "hashicups_coffees" "all" {}
-
-resource "hashicups_order" "order" {
-  items {}
 }
--- tofu_tf_dir/elem2_test.tf
+++ tofu_tf_dir/elem2_test.tf
@@ -1,4 +1,4 @@
 output "selected_instance_type2" {
   description = "The selected instance type from the list using custom element2 function"
-  value       = element(["a", "b", "c"], length(["a", "b", "c"])-1)
+  value       = element(["a", "b", "c"], length(["a", "b", "c"]) - 1)
 }
